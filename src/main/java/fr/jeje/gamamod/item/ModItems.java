8e31f9d4cc35f446eecf498a4c6ad15a729e803d
Jeje <100199805+JejeMC0@users.noreply.github.com>
2022-04-10 12:44:30 +0200

    Add files via upload

--- src/main/java/fr/jeje/gamamod/item/ModItems.java
+++ src/main/java/fr/jeje/gamamod/item/ModItems.java
@@ -16,7 +16,23 @@ public class ModItems {
     //skateboard
     public static final RegistryObject<Item> SKATEBOARD = ITEMS.register("skateboard",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SKATEBOARD_TAB).stacksTo(1)));
-    
+    //skateboard deck
+    public static final RegistryObject<Item> SKATEBOARD_DECK = ITEMS.register("skateboard_deck",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SKATEBOARD_TAB).stacksTo(1)));
+
+    //wheel
+    public static final RegistryObject<Item> WHEEL = ITEMS.register("wheel",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SKATEBOARD_TAB).stacksTo(16)));
+
+    //trucks
+    public static final RegistryObject<Item> TRUCKS = ITEMS.register("trucks",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SKATEBOARD_TAB).stacksTo(8)));
+
+    //screw
+    public static final RegistryObject<Item> SCREW = ITEMS.register("screw",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SKATEBOARD_TAB).stacksTo(32)));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
