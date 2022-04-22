0208d3f9bccc2483dde5a5a88e3b6d8a45a87585
Jeje <100199805+JejeMC0@users.noreply.github.com>
2022-04-22 20:08:34 +0200

    Add files via upload

--- src/main/java/fr/jeje/gamamod/GamaMod.java
+++ src/main/java/fr/jeje/gamamod/GamaMod.java
@@ -3,7 +3,10 @@ package fr.jeje.gamamod;
 
 import com.mojang.logging.LogUtils;
 
+import fr.jeje.gamamod.block.ModBlocks;
 import fr.jeje.gamamod.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -25,10 +28,18 @@ public class GamaMod {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+
+    private void clientSetup(final FMLCommonSetupEvent event){
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.SKATEBOARD_ASSEMBLER.get(), RenderType.cutout());
+
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/fr/jeje/gamamod/block/ModBlocks.java
@@ -0,0 +1,52 @@
+package fr.jeje.gamamod.block;
+
+import fr.jeje.gamamod.CreativeTab.ModCreativeModeTab;
+import fr.jeje.gamamod.GamaMod;
+import fr.jeje.gamamod.block.custom.Skateboard_assembler;
+import fr.jeje.gamamod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, GamaMod.MOD_ID);
+
+
+    public static final RegistryObject<Block> SKATEBOARD_ASSEMBLER = registerBlock("skateboard_assembler",
+            () -> new Skateboard_assembler(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()),
+            ModCreativeModeTab.SKATEBOARD_TAB);
+
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+
+    }
+
+
+    private static <T extends Block>RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                           CreativeModeTab tab){
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+
+
+    }
+}
